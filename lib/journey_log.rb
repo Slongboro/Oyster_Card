42582c7f477ccf85c1ee5622d707a4c1ee96dcac
slongboro <susanlongley@gmail.com>
2019-10-24 16:31:47 +0100

    final push for starts a journey test / code, failing

---
+++ lib/journey_log.rb
@@ -0,0 +1,13 @@
+require 'journey'
+
+class JourneyLog
+
+    def initialize(journey_class: )
+        @journey_class = journey_class
+        @journeys = []
+      end
+
+      def start(station)
+        @journey_class
+      end
+end
